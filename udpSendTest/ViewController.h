57d2457b033c69028633791f593b917b3fe07c7f
Anthony Chollet <anthonychollet@MacBook-Pro-de-anthony.local>
2016-02-16 16:16:58 +0100

    add project udpSendTest
    ajout du projet iOS (iPhone) pour envoyer un message udp

--- udpSendTest/ViewController.h
+++ udpSendTest/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UITextField *tbText;
+@property (weak, nonatomic) IBOutlet UIButton *btnSend;
 
 @end
 
--- udpSendTest/ViewController.m
+++ udpSendTest/ViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "ViewController.h"
+#import <sys/socket.h>
+#import <netinet/in.h>
+#import <arpa/inet.h>
+
+#define Port 80
+#define IpAddress 155.0.0.1
 
 @interface ViewController ()
-
+@property (nonatomic) CFSocketRef udpSocket;
 @end
 
 @implementation ViewController
@@ -17,11 +23,49 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    _udpSocket = CFSocketCreate(kCFAllocatorDefault, PF_INET, SOCK_DGRAM, IPPROTO_UDP, 0, NULL, NULL);
+    if(_udpSocket){
+        NSLog(@"socket create");
+        
+        struct sockaddr_in addr;
+        memset(&addr, 0, sizeof(addr));
+        addr.sin_len = sizeof(addr);
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(8080); // PORT
+        inet_aton("127.0.0.1", &addr.sin_addr); // IP ADDRESS
+        
+        NSData *address = [NSData dataWithBytes:&addr length:sizeof(addr)];
+        if(CFSocketSetAddress(_udpSocket, (CFDataRef)address) != kCFSocketSuccess){
+            NSLog(@"erreur set address");
+        }
+        else{
+            NSLog(@"set address OK");
+            CFSocketConnectToAddress(_udpSocket, (CFDataRef)address, 10);
+        }
+    }
+    else{
+        NSLog(@"error socket creation");
+    }
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickSendBtn:(id)sender {
+    NSString *message = _tbText.text;
+    NSData *message_data = [message dataUsingEncoding:NSUTF8StringEncoding];
+
+    CFSocketError socket_error = CFSocketSendData(_udpSocket, NULL, (CFDataRef)message_data, 10);
+    
+    if(socket_error < 0){
+        NSLog(@"Error send");
+    }
+    else{
+        NSLog(@"send ok");
+    }
 }
 
 @end
